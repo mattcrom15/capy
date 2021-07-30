01493945d94b6b9e3f1a0288e88bce5a9275ff05
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2021-07-30 20:05:55 +0100

    custom play/pause button

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,10 +1,17 @@
+import React, { useState } from 'react'
 import ReactPlayer from 'react-player'
-
+import Button from './Button'
 
 
 const VideoPlayer = (props) => {
+    const [Playing,setPlaying] = useState(false);
     return (
-        <ReactPlayer url={props.videoFile} controls = {true}/>
+        <div>
+            <ReactPlayer url={props.videoFile} playing={Playing}/>
+            <Button onClick={() => setPlaying(true)}>Play</Button>
+            <Button onClick={() => setPlaying(false)}>Pause</Button>
+            <Button>New Cue</Button>
+        </div>
     );
   };
   
