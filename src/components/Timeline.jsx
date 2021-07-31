3f71616422af80a31d9452f385d5083e466bbc46
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2021-07-31 20:51:26 +0100

    basic timeline scrubbing

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Timeline = (props) => {
     return (
         <div>
-            <div className = "timeline-slider" onMouseDown={props.MouseDown} onMouseUp={props.onMouseUp}>
+            <div className = "timeline-slider" onMouseDown={props.MouseDown} onMouseUp={props.onMouseUp} onChange={props.seekChange} onMouseMove={props.mm}>
                 <div className = "slider-border">
                     <div className = "slider-progress"  style = {{width: props.value}} ></div>
                     <div className="slider-playhead"></div>
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -9,7 +9,9 @@ const VideoPlayer = (props) => {
     const [Time,setTime] = useState();
     const [VideoDuration, setVideoDuration] = useState();
     const [OnProgress, setOnProgress] = useState('playedSeconds');
-    const [seeking,setSeeking] = useState(false)
+    const [Seeking,setSeeking] = useState(false)
+    const [Shit,setShit] = useState(0)
+    const [MouseMove, setMouseMove] = useState({"x": 0, "y": 0})
     
     const player = React.createRef();
 
@@ -19,31 +21,43 @@ const VideoPlayer = (props) => {
       return time
     } 
 
-    function SeekTime(){
-      const seek = player.current.seekTo(Time,'seconds')
-      setOnProgress(seek)
-    }
-
     function restart(){
       player.current.seekTo(0,'seconds')
       setTime(0)
       setOnProgress('playedSeconds')
     }
 
+    function seekChange(e,j) {
+      const MouseLocation =  j
+      if(Seeking == true){
+        console.log(j)
+        player.current.seekTo(MouseLocation,'seconds')
+        setTime(MouseLocation)
+        
+        
+      }
+    
+    }
+
     return (
         <div>
-            <ReactPlayer url={props.videoFile} playing={Playing} ref={player} onDuration={setVideoDuration} onProgress={setOnProgress}/>
+            <ReactPlayer url={props.videoFile} 
+            playing={Playing} 
+            ref={player} 
+            onDuration={setVideoDuration} onProgress={setOnProgress}/>
             <Timeline value={OnProgress['playedSeconds']}
-                    max={VideoDuration} 
-                    MouseDown={() =>setSeeking(true)}
-                    onMouseUp={()=> setSeeking(false)}/>
+            max={VideoDuration} 
+            MouseDown={() =>setSeeking(true)}
+            seekChange={()=> seekChange(this)}
+            onMouseUp={()=> setSeeking(false)}
+            mm={(e)=>seekChange(this,e.nativeEvent.offsetX)}/>
             <Button onClick={() => setPlaying(true)}>Play</Button>
             <Button onClick={() => setPlaying(false)}>Pause</Button>
             <Button onClick={() => CurrentTime()}>set time</Button>
             <Button onClick={() => restart()}>Back</Button>
             <h1>{Time}</h1>
             <h1>{VideoDuration}</h1>
-            <p>{String(seeking)}</p>
+            <p>{String(Seeking)}</p>
         </div>
     );
   };
